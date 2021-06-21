7d2e239492d627364c0306c9cd3ea8cb0823bc36
jia0_nn <linao264590000@gmail.com>
2021-06-21 19:20:46 +0800

    get the pages only related to Sales

--- mobo/spiders/mobospider.py
+++ mobo/spiders/mobospider.py
@@ -6,10 +6,13 @@ from scrapy.spiders import CrawlSpider, Rule
 class MobospiderSpider(CrawlSpider):
     name = 'mobospider'
     allowed_domains = ['www.mo-bo.com.tw']
+
+    # 進入點
     start_urls = ['http://www.mo-bo.com.tw/']
 
+    # 抓「特價」相關的頁面
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'https://www.mo-bo.com.tw/PDSale'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
